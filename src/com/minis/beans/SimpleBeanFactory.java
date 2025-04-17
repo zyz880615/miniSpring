4bedf420b78c47e8bb5d8b514e81e90e5da63f05
zyz880615 <zyz880615@163.com>
2025-04-17 13:51:31 +0800

    提交完整属性注入、构造器注入代码

--- src/com/minis/beans/SimpleBeanFactory.java
+++ src/com/minis/beans/SimpleBeanFactory.java
@@ -1,5 +1,7 @@
 package com.minis.beans;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +14,105 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
     public SimpleBeanFactory() {
     }
 
+    private Object createBean(BeanDefinition beanDefinition) {
+        Class<?> clz = null;
+        Object obj = null;
+        Constructor<?> con = null;
+        try {
+            clz = Class.forName(beanDefinition.getClassName());
+            // 处理构造器参数
+            ArgumentValues argumentValues =
+                    beanDefinition.getConstructorArgumentValues();
+            //如果有参数
+            if (!argumentValues.isEmpty()) {
+                Class<?>[] paramTypes = new Class<?>
+                        [argumentValues.getArgumentCount()];
+                Object[] paramValues = new
+                        Object[argumentValues.getArgumentCount()];
+                //对每一个参数，分数据类型分别处理
+                for (int i = 0; i < argumentValues.getArgumentCount(); i++) {
+                    ArgumentValue argumentValue =
+                            argumentValues.getIndexedArgumentValue(i);
+                    if ("String".equals(argumentValue.getType()) ||
+                            "java.lang.String".equals(argumentValue.getType())) {
+                        paramTypes[i] = String.class;
+                        paramValues[i] = argumentValue.getValue();
+                    } else if ("Integer".equals(argumentValue.getType()) ||
+                            "java.lang.Integer".equals(argumentValue.getType())) {
+                        paramTypes[i] = Integer.class;
+                        paramValues[i] =
+                                Integer.valueOf((String) argumentValue.getValue());
+                    } else if ("int".equals(argumentValue.getType())) {
+                        paramTypes[i] = int.class;
+                        paramValues[i] = Integer.valueOf((String)
+                                argumentValue.getValue());
+                    } else { //默认为string
+                        paramTypes[i] = String.class;
+                        paramValues[i] = argumentValue.getValue();
+                    }
+                }
+                try {
+                    //按照特定构造器创建实例
+                    con = clz.getConstructor(paramTypes);
+                    obj = con.newInstance(paramValues);
+                } catch (Exception e) {
+
+                }
+            } else { //如果没有参数，直接创建实例
+                obj = clz.getDeclaredConstructor().newInstance();
+            }
+        } catch (Exception e) {
+        }
+        // 处理属性
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        if (!propertyValues.isEmpty()) {
+            for (int i = 0; i < propertyValues.size(); i++) {
+                //对每一个属性，分数据类型分别处理
+                PropertyValue propertyValue =
+                        propertyValues.getPropertyValueList().get(i);
+                String pType = propertyValue.getType();
+                String pName = propertyValue.getName();
+                Object pValue = propertyValue.getValue();
+                Class<?>[] paramTypes = new Class<?>[1];
+                if ("String".equals(pType) || "java.lang.String".equals(pType)) {
+                    paramTypes[0] = String.class;
+                } else if ("Integer".equals(pType) ||
+                        "java.lang.Integer".equals(pType)) {
+                    paramTypes[0] = Integer.class;
+                } else if ("int".equals(pType)) {
+                    paramTypes[0] = int.class;
+                } else { // 默认为string
+                    paramTypes[0] = String.class;
+                }
+                Object[] paramValues = new Object[1];
+                paramValues[0] = pValue;
+
+                //按照setXxxx规范查找setter方法，调用setter方法设置属性
+                String methodName = "set" + pName.substring(0, 1).toUpperCase()
+                        + pName.substring(1);
+                Method method = null;
+                try {
+                    method = clz.getMethod(methodName, paramTypes);
+                } catch (Exception e) {
+
+                }
+                try {
+                    method.invoke(obj, paramValues);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        return obj;
+    }
+
+
     //getBean，容器的核心方法
     public Object getBean(String beanName) throws BeansException {
         //先尝试直接拿Bean实例
-        Object singleton = this.getSingleton(beanName);
+        Object bean = this.getSingleton(beanName);
         //如果此时还没有这个Bean的实例，则获取它的定义来创建实例
-        if (singleton == null) {
+        if (bean == null) {
 
             //获取Bean的定义
             BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
@@ -26,17 +121,13 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
                 throw new BeansException("No bean");
             }
 
-            try {
-                singleton = Class.forName(beanDefinition.getClassName()).newInstance();
-            } catch (Exception e) {
-
-            }
+            bean = createBean(beanDefinition);
 
             //注册Bean实例
-            this.registerSingleton(beanName, singleton);
+            this.registerSingleton(beanName, bean);
 
         }
-        return singleton;
+        return bean;
     }
 
     @Override
